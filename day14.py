ccec8dac196ae1a79e50177ac6f5c4ca9915cef3
pwnro <nutzudroid@gmail.com>
2015-12-14 13:49:31 +0000

    day14 part 1, reindeers!

---
+++ day14.py
@@ -0,0 +1,44 @@
+import sys
+from math import ceil
+
+#f = 'day14.input.txt'
+f = 'day14.sample.txt'
+
+try:
+    lines = tuple(open(f, 'r'))
+except:
+    sys.exit("can't open %s" % f)
+    
+reindeers = []
+for line in lines:
+    words = line.strip().split()
+    reindeers.append({
+        'name': words[0],
+        'speed': int(words[3]),     # km/s
+        'burst': int(words[6]),     # s
+        'rest': int(words[-2])      # s
+    })
+
+race_len = 2503 
+
+distances = []
+for reindeer in reindeers:    
+    burst = reindeer['burst']
+    speed = reindeer['speed']
+    rest = reindeer['rest']
+
+    # durata unui hop in secunde
+    hop_s = burst + rest
+    
+    # distanta parcursa per hop, d = t x v
+    hop_d = burst * speed
+    
+    # numarul de hopuri (rotunjit superior)
+    hops = ceil(race_len / hop_s)
+    
+    # distanta parcursa, d = hop_d * hops
+    distance = hop_d * hops
+    distances.append(distance)
+    print("%s termina in %d hopuri, parcurgand distanta %d" % (reindeer['name'], hops, distance))
+
+print("max = %d" % max(distances))
